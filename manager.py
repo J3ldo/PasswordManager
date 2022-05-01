827456eb2191736a5ee927fd206bf4b4bee14ca9
J3ldo <97303347+J3ldo@users.noreply.github.com>
2022-05-01 16:52:50 +0200

    Added a checker feature

--- manager.py
+++ manager.py
@@ -61,6 +61,68 @@ def remove_pass(index):
     print("Succesfully removed the item from the list.")
 
 
+chars = "qwertyuiopasdfghjklzxcvbnm"
+nums = "1234567890"
+spec_chars = "`~!@#$%^&*()-_=+{[}]|\\:;\"',<.>/?"
+all_chars = [list(chars), list(chars.upper()), list(nums), list(spec_chars)]
+def check_pass(passw):
+    strength = 0
+    if len(passw) >= 12:
+        strength += 5
+
+    else:
+        strength -= 3
+    contains = {"letter": 0, "nums": 0, "chars": 0, "upper": 0}
+    #letters: 10, nums: 3, char: 1, upper: 2
+
+    for i in all_chars[0]:
+        for char in list(passw):
+            if i == char:
+                contains["letter"] += 1
+
+    for i in all_chars[1]:
+        for char in list(passw):
+            if i == char:
+                contains["upper"] += 1
+
+    for i in all_chars[2]:
+        for char in list(passw):
+            if i == char:
+                contains["nums"] += 1
+
+    for i in all_chars[3]:
+        for char in list(passw):
+            if i == char:
+                contains["chars"] += 1
+
+    if contains["letter"] >= 10:
+        strength += 3
+
+    if contains["upper"] >= 2:
+        strength += 2
+
+    if contains["nums"] >= 3:
+        strength += 2
+
+    if contains["chars"] >= 1:
+        strength += 4
+
+    if strength >= 16:
+        print("Your password is super strong. It meets all requirements")
+
+    elif strength <= 0:
+        print("Your password doesn't meet any requirements. Please don't use it")
+
+    elif strength >= 13:
+        print("A nice password to use. It meets almost all requirements")
+
+    elif strength >= 9:
+        print("Medium password not to strong but also not easy to crack.")
+
+    else:
+        print("A easy/medium password. Use at your own risk")
+
+
 print("Type help for a lists of commands. Type q or quit to quit")
 cmd = ""
 while cmd != "q" or cmd != "quit":
@@ -72,6 +134,7 @@ while cmd != "q" or cmd != "quit":
               "add - To add a password.\n"
               "remove - To remove a password\n"
               "help - To show all commands\n"
+              "check - To check on password strength\n"
               "quit - To quit this program.")
 
     elif cmd == "view":
@@ -82,6 +145,9 @@ while cmd != "q" or cmd != "quit":
 
     elif cmd == "remove":
         remove_pass(int(input("Index of password: ")))
+
+    elif cmd == "check":
+        check_pass(input("Password: "))
 
     elif cmd == "q" or cmd == "quit":
         print("Quitting")
